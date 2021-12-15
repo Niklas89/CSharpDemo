30b754f7bc70a8ac93d43fd60c2da574f7e167c8
Niklas <niklas_edelstam@hotmail.com>
2021-12-15 22:05:11 +0100

    modif du code et ajout dexceptions

--- CSharpIntro/Partie1/ConsoleApp6/ConsoleApp6/Program.cs
+++ CSharpIntro/Partie1/ConsoleApp6/ConsoleApp6/Program.cs
@@ -2,16 +2,52 @@
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 string lastName;
 string firstName;
-int dateBirth;
-// Demander à l'utilisateur son prénom
-Console.WriteLine("Quel est ton prénom?");
-firstName = Console.ReadLine();
-// Demander à l'utilisateur son nom
-Console.WriteLine("Quel est ton nom?");
-lastName = Console.ReadLine();
-// Demander à l'utilisateur sa date de naissance
-Console.WriteLine("Quel est ton annee de naissance?");
-string dateBirthString = Console.ReadLine();
-dateBirth = int.Parse(dateBirthString);
-// Afficher les renseignements avec les détails entrés ci-dessus
-Console.WriteLine("Fiches de renseignement \n * Nom: " + lastName + "\n * Prenom: " + firstName + "\n * Age : " + dateBirth);
+string dateBirthString;
+int dateBirth = 0;
+Console.WriteLine("Ecrire 'Quit' pour quitter le program.");
+Console.WriteLine("Vous devez remplir toutes les cases sinon il faut recommencer.");
+Boolean keepAsking = true;
+do
+{
+    // Demander à l'utilisateur son prénom
+    Console.WriteLine("Quel est ton prénom?");
+    firstName = Console.ReadLine();
+    if(firstName == "Quit")
+    {
+        keepAsking = false;
+    } else
+    {
+        // Demander à l'utilisateur son nom
+        Console.WriteLine("Quel est ton nom?");
+        lastName = Console.ReadLine();
+        if(lastName == "Quit")
+        {
+            keepAsking = false;
+        } else
+        {
+            try
+            {
+                // Demander à l'utilisateur sa date de naissance
+                Console.WriteLine("Quel est ton annee de naissance?");
+                dateBirthString = Console.ReadLine();
+                if (dateBirthString == "Quit")
+                {
+                    keepAsking = false;
+                } else if(firstName == "" || lastName == "") {
+                    Console.WriteLine("Vous n'avez pas rempli toutes les cases, vous devez recommencer. \n");
+                    keepAsking = true;
+                } else
+                {
+                    dateBirth = int.Parse(dateBirthString);
+                    // Afficher les renseignements avec les détails entrés ci-dessus
+                    Console.WriteLine("Fiches de renseignement \n * Nom: " + lastName + "\n \n * Prenom: " + firstName + "\n \n * Age : " + dateBirth);
+                    keepAsking = false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Veuillez entrer un nombre pour la date de naissance svp. \n");
+            }
+        }
+    }
+} while (keepAsking);
