2f40d1357c0f70e9a33467f7bdb696e6859d4648
Niklas <niklas_edelstam@hotmail.com>
2021-12-15 16:10:13 +0100

    modifs du code

--- CSharpIntro/Partie3/ConsoleApp4/ConsoleApp4/Program.cs
+++ CSharpIntro/Partie3/ConsoleApp4/ConsoleApp4/Program.cs
@@ -2,29 +2,56 @@
 Random randomNumber = new Random();
 // Définir un nombre aléatoire entre 1 et 50
 int numberToGuess = randomNumber.Next(1,50);
-int guessedNumber;
 Boolean keepGuessing = true;
 int attempt = 0; // nombre de tentatives pour trouver le nombre
 // Demander à l’utilisateur de deviner ce nombre, continuer à demande tant que qu'il n'a pas trouvé
-while(keepGuessing)
+Console.WriteLine("Ecrivez 'Quit' pour quitter le program directement.");
+while (keepGuessing)
 {
+    int guessedNumber = 0;
     Console.WriteLine("Devinez le nombre: ");
     string guessedNumberString = Console.ReadLine();
-    guessedNumber = int.Parse(guessedNumberString);
-    if (guessedNumber == numberToGuess)
+    // Taper "Quit" pour quitter le program
+    if (guessedNumberString == "Quit")
     {
-        Console.WriteLine("Bravo vous avez trouvé !");
         keepGuessing = false;
     }
-    else if (guessedNumber <= numberToGuess)
+    // Vérifier si l'utilisateur a bien entré un nombre
+    try
     {
-        Console.WriteLine("C’est plus grand");
+        guessedNumber = int.Parse(guessedNumberString);
+        // Vérifier que le nombre deviné se situe bien entre 1 et 50
+        if ((guessedNumber >= 1) && (guessedNumber <= 50))
+        {
+            // Si le nombre deviné est bien égal au nombre défini, affiche le message "bravo"
+            if (guessedNumber == numberToGuess)
+            {
+                Console.WriteLine("Bravo vous avez trouvé !");
+                keepGuessing = false;
+            }
+            // Si le nombre deviné est inférieur ou égal au nombre défini
+            else if (guessedNumber <= numberToGuess)
+            {
+                Console.WriteLine("C’est plus grand");
+                attempt++;
+                Console.WriteLine("Tentative : " + attempt + "\n");
+            }
+            // Sinon le nombre deviné peut uniquement être plus grand que le nombre défini
+            else
+            {
+                Console.WriteLine("C’est plus petit");
+                attempt++;
+                Console.WriteLine("Tentative : " + attempt + "\n");
+            }
+        }
+        else
+        {
+            Console.WriteLine("\n Le nombre deviné doit se situer entre 1 et 50. Veuillez réessayer.");
+        }
+    } catch (Exception ex)
+    {
+        Console.WriteLine("\n Vous n'avez pas entré un nombre, veuillez essayer à nouveau");
     }
-    else
-    {
-        Console.WriteLine("C’est plus petit");
-    } 
-    attempt++;
-    Console.WriteLine("Tentative : " + attempt);
+    
 }
 Console.WriteLine("Tentatives au total : " + attempt);
