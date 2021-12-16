0f1a263d4e2027a6bf74b06b7092530208be8304
Niklas <niklas_edelstam@hotmail.com>
2021-12-16 11:17:54 +0100

    ajout de try catch dans le code

--- CSharpIntro/Partie3/ConsoleApp3/ConsoleApp3/Program.cs
+++ CSharpIntro/Partie3/ConsoleApp3/ConsoleApp3/Program.cs
@@ -5,21 +5,29 @@ string numberString;
 Boolean keepAsking = true;
 while(keepAsking)
 {   
-    // demander à l'utilisateur d'entrer un nombre à additionner
-    Console.WriteLine("Entrez un nombre:");
-    numberString = Console.ReadLine();
-    number = int.Parse(numberString);
-    result += number;
-    // demander à l'utilisateur s'il veut ajouter encore un nombre
-    Console.WriteLine("Ajouter encore un nombre?");
-    string addNumber = Console.ReadLine();
-    // Tant que l’utilisateur ne répond pas non le program continue à demander un nombre
-    if (addNumber == "non"){
-        keepAsking = false;
-    } else
+    try
     {
-        keepAsking = true;
+        // demander à l'utilisateur d'entrer un nombre à additionner
+        Console.WriteLine("Entrez un nombre:");
+        numberString = Console.ReadLine();
+        number = int.Parse(numberString);
+        result += number;
+        // demander à l'utilisateur s'il veut ajouter encore un nombre
+        Console.WriteLine("Ajouter encore un nombre?");
+        string addNumber = Console.ReadLine();
+        // Tant que l’utilisateur ne répond pas non le program continue à demander un nombre
+        if (addNumber == "non")
+        {
+            keepAsking = false;
+        }
+        else
+        {
+            keepAsking = true;
+        }
+    } catch (Exception ex)
+    {
+        Console.WriteLine("Vous n'avez pas entré un nombre, veuillez réessayer.");
     }
 }
-Console.WriteLine("Le resultat est "+result);
+Console.WriteLine("Le resultat est " + result);
 
