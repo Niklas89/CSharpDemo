bdcb8c7dd85a76c5585c534c06ec6fd908195a79
Niklas <niklas_edelstam@hotmail.com>
2021-12-16 10:42:03 +0100

    modifs code

--- CSharpIntro/Partie1/ConsoleApp5/ConsoleApp5/Program.cs
+++ CSharpIntro/Partie1/ConsoleApp5/ConsoleApp5/Program.cs
@@ -2,17 +2,48 @@
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 string lastName;
 string firstName;
-// Demander le prénom à l'utilisateur
-Console.WriteLine("Quel est ton prénom?");
-firstName = Console.ReadLine();
-// Demander le nom à l'utilisateur
-Console.WriteLine("Quel est ton nom?");
-lastName = Console.ReadLine();
-// Calculer la date actuelle
-DateTime today = DateTime.Now;
-int year = today.Year;
-int month = today.Month; 
-int day = today.Day; 
-// Afficher le message final avec nom, prénom et date
-Console.WriteLine("Bonjour " + lastName + " " + firstName + ", nous sommes le "+day + "/" + month + "/" + year+", comment allez-vous ?");
+Boolean keepAsking = true;
+Console.WriteLine("Ecrivez 'Quit' pour quitter le program.");
+// Boucle while pour quitter le program si jamais l'utilisateur rentre "Quit" ou recommencer s'il ne rentre rien
+do
+{
+    // Demander le prénom à l'utilisateur
+    Console.WriteLine("Quel est ton prénom?");
+    firstName = Console.ReadLine();
+    if (firstName == "Quit")
+    {
+        keepAsking = false;
+    }
+    else if (firstName == "" || firstName == " ")
+    {
+        Console.WriteLine("Vous devez mettre du texte !");
+        keepAsking = true;
+    }
+    else
+    {
+        // Demander le nom à l'utilisateur
+        Console.WriteLine("Quel est ton nom?");
+        lastName = Console.ReadLine();
+        if (lastName == "Quit")
+        {
+            keepAsking = false;
+        }
+        else if (lastName == "" || lastName == " ")
+        {
+            Console.WriteLine("Vous devez mettre du texte !");
+            keepAsking = true;
+        }
+        else
+        {
+            // Calculer la date actuelle 
+            DateTime today = DateTime.Now;
+            int year = today.Year;
+            int month = today.Month;
+            int day = today.Day;
+            // Afficher le message final avec nom, prénom et date
+            Console.WriteLine("Bonjour " + lastName + " " + firstName + ", nous sommes le " + day + "/" + month + "/" + year + ", comment allez-vous ?");
+            keepAsking = false;
 
+        }
+    }
+} while (keepAsking);
