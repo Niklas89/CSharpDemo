689129f6c02b1374c33db4031178afc14eaf24b7
Niklas <niklas_edelstam@hotmail.com>
2021-12-16 12:08:52 +0100

    modifs du code dans l'exo 5

--- CSharpIntro/Partie4/ConsoleApp5/ConsoleApp5/Program.cs
+++ CSharpIntro/Partie4/ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+bool keepAskingTornado = true;
 string[,] fujita = new string[6, 2]
 {
     {"F0","Dégâts légers : certains dommages sont subis par les cheminées, les antennes de télévision, les bardeaux, les arbres, les enseignes et les fenêtres." },
@@ -8,17 +9,23 @@ string[,] fujita = new string[6, 2]
     {"F4", "Dégâts dévastateurs : même dans les habitations solides, l'essentiel des murs, sinon tous, s'effondrent ; tels des missiles, de gros objets en acier ou en béton sont projetés à grandes distances." },
     {"F5", "Dégâts incroyables : les maisons sont rasées ou projetées sur de grandes distances. Les tornades F5 peuvent causer des dommages très importants à de grosses structures telles que les écoles et les motels et peuvent arracher les murs extérieurs et les toits (parfois surnommé « le doigt de Dieu »." },
 };
-// Inviter l’utilisateur à renseigner le type d’une tornade
-Console.WriteLine("Quel type de tornade souhaitez-vous consulter ?");
-string type = Console.ReadLine();
-// Afficher la description des dégâts causés par le type de tornade 
-for (int i = 0; i < 6; i++)
-{
-    for(int j= 0; j < 2; j++)
-    {
-        if (fujita[i, j] == type)
+// Avec une boucle while vérifier si l'utilisateur rentre bien le type de la tornade, quitter le programme s'il rentre "Quit"
+Console.WriteLine("Ecrivez 'Quit' pour quitter le program.");
+while(keepAskingTornado) {
+    // Inviter l’utilisateur à renseigner le type d’une tornade
+    Console.WriteLine("Quel type de tornade souhaitez-vous consulter ?");
+    string tornadoType = Console.ReadLine();
+    if(tornadoType == "Quit") {
+        keepAskingTornado = false;
+    } else {
+        // Afficher la description des dégâts causés par le type de tornade 
+        for (int i = 0; i < 6; i++)
         {
-            Console.WriteLine(fujita[i, 1]);
+            if (fujita[i, 0] == tornadoType)
+            {
+                Console.WriteLine(fujita[i, 1]);
+                keepAskingTornado = false;
+            } 
         }
     }
 }
